374e434c0433af5125adc1f82b71d2fcdb01c06f
Wilson Chan <wilsonchan54@gmail.com>
2022-05-23 15:33:51 -0400

    Created searchPhotos function based off curatedPhotos (default) / used event listener to show successful search

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // Variables
-const auth = `${API_KEY}`;
+const auth = ;
 const loadMore = document.querySelector(".loadMore");
 const input = document.querySelector("input");
 const searchButton = document.querySelector(".searchButton");
@@ -8,10 +8,12 @@ let pageNum = 1;
 let search = false;
 let query = "";
 
+
 input.addEventListener("input", (e)=>{
     e.preventDefault();        // refresh doesn't remove data
     query = e.target.value;
 });
+
 
 // Starting images after loading
 async function curatedPhotos(pageNum){
@@ -36,5 +38,39 @@ async function curatedPhotos(pageNum){
         document.querySelector(".gallery").appendChild(pic);
     });
 }
+
+
+// Searching Images
+async function searchPhotos(query, pageNum){
+    const data = await fetch(`"https://api.pexels.com/v1/search?query=${query}&per_page=15&page=${pageNum}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: auth,
+        },
+    }    
+    );
+    const result = await data.json();
+
+    // Data to render from API
+    result.photos.forEach(photo =>{
+        const pic = document.createElement("div");
+        pic.innerHTML = `<img src=${photo.src.large}>
+        <p>Photo : ${photo.photographer}</p>
+        <a href= ${photo.src.large}>Download<a/>
+        `;
+        document.querySelector(".gallery").appendChild(pic);
+    });
+}
+
+// Indicate that search worked
+searchButton.addEventListener("click", ()=>{
+    if (input.value === "") return;
+    clear();
+    search = true;
+    searchPhotos(query, pageNum);
+});
+
+
 
 curatedPhotos(pageNum);
